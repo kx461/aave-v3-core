40c19cf7e355da09ecbcc4bee9e5ba810f97473a
kx461 <31888758+kx461@users.noreply.github.com>
2022-11-06 17:24:29 +0200

    Update FlashLoanSimpleReceiverBase.sol

--- contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol
+++ contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.10;
 
-import {IFlashLoanSimpleReceiver} from '../interfaces/IFlashLoanSimpleReceiver.sol';
 import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
 import {IPool} from '../../interfaces/IPool.sol';
 
@@ -10,7 +9,7 @@ import {IPool} from '../../interfaces/IPool.sol';
  * @author Aave
  * @notice Base contract to develop a flashloan-receiver contract.
  */
-abstract contract FlashLoanSimpleReceiverBase is IFlashLoanSimpleReceiver {
+abstract contract FlashLoanSimpleReceiverBase {
   IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
   IPool public immutable override POOL;
 
